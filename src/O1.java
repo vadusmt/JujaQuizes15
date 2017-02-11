1470ae77274ba96904a2c1b85abef0993703a040
vadusmt <vadusmt@gmail.com>
2017-02-11 22:20:02 +0200

    Initial commit

---
+++ src/O1.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Vadus on 11.02.2017.
+ */
+public class O1 {
+}
+class ParentMessage {
+    void call() {
+        System.out.println("Parent");
+    }
+}
+class ChildMessage extends ParentMessage {
+    void call() {
+        System.out.println("Child");
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        send(new ChildMessage());
+    }
+    public static void send(ParentMessage msg) {
+        msg.call();
+    }
+}
